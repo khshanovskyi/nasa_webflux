6ba23006c45aa67bddea038803155c2711734f32
Pavlo Khshanovskyi <pavlo_khshanovskyi@epam.com>
2022-07-19 21:01:56 +0300

    Initial commit

---
+++ src/main/java/com/example/nasa_webflux/NasaWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.nasa_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasaWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nasa_webflux/NasaWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nasa_webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
